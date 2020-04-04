504c573bad35b559e949b92655b026fe0dcce205
Shahzad Umar Baig <ShahzadUBaig@gmail.com>
2020-04-04 16:22:45 +0500

    Shahzad Umar Baig Changede JS

--- store.js
+++ store.js
@@ -1,307 +1,290 @@
-
 //sroll
 var marginY = 0;
 var destination = 0;
 var speed = 30;
 var scroller = null;
 
-function ScrollFun(element){
-    destination = document.getElementById(element).offsetTop;
+function ScrollFun(element) {
+  destination = document.getElementById(element).offsetTop;
 
-    scroller = setTimeout(function(){
-        ScrollFun(element);
-    },7)
+  scroller = setTimeout(function() {
+    ScrollFun(element);
+  }, 7);
 
-    marginY = marginY + speed;
+  marginY = marginY + speed;
 
-    if(marginY >= destination){
-        clearTimeout(scroller);
-    }
-    window.scroll( 0, marginY);
+  if (marginY >= destination) {
+    clearTimeout(scroller);
+  }
+  window.scroll(0, marginY);
 }
 
 //item-numbers
 
-const button = document.querySelectorAll('.btn');
+const button = document.querySelectorAll(".btn");
 
 let product = [
-    {
-        name: 'you can shop your favourite item' , 
-        price : 0 , 
-        tag : 'shop', 
-        Incart : 0
-    },
-    {
-        name: 'chocolate cupcake' , 
-        price : 90 , 
-        tag : 'cc', 
-        Incart : 0
-    },
+  {
+    name: "you can shop your favourite item",
+    price: 0,
+    tag: "shop",
+    Incart: 0
+  },
+  {
+    name: "chocolate cupcake",
+    price: 90,
+    tag: "cc",
+    Incart: 0
+  },
 
-    {
-        name: 'Vanila cupcake' , 
-        price : 
-        90 , 
-        tag : 'vc', 
-        Incart : 0
-    },
+  {
+    name: "Vanila cupcake",
+    price: 90,
+    tag: "vc",
+    Incart: 0
+  },
 
-    {
-        name: 'oreo cupcake' , 
-        price : 90 , 
-        tag : 'oc', 
-        Incart : 0
-    },
-    {
-        name: 'strawberry cupcake' , 
-        price : 90 , 
-        tag : 'sc', 
-        Incart : 0
-    },
+  {
+    name: "oreo cupcake",
+    price: 90,
+    tag: "oc",
+    Incart: 0
+  },
+  {
+    name: "strawberry cupcake",
+    price: 90,
+    tag: "sc",
+    Incart: 0
+  },
 
-    {
-        name: 'belgium cupcake' , 
-        price : 90 , 
-        tag :'bc', 
-        Incart : 0
-    },
+  {
+    name: "belgium cupcake",
+    price: 90,
+    tag: "bc",
+    Incart: 0
+  },
 
-    {
-        name: 'caramel cupcake' , 
-        price : 90 , 
-        tag : 'crc', 
-        Incart : 0
-    },
+  {
+    name: "caramel cupcake",
+    price: 90,
+    tag: "crc",
+    Incart: 0
+  },
 
-    {
-        name: 'jamdot' , 
-        price : 150 , 
-        tag : 'jd', 
-        Incart : 0
-    },
+  {
+    name: "jamdot",
+    price: 150,
+    tag: "jd",
+    Incart: 0
+  },
 
-    {
-        name: 'chocolate ' , 
-        price : 170 , 
-        tag : 'cb', 
-        Incart : 0
-    },
+  {
+    name: "chocolate ",
+    price: 170,
+    tag: "cb",
+    Incart: 0
+  },
 
-    {
-        name: 'nuts' , 
-        price : 160 , 
-        tag : 'nb', 
-        Incart : 0
-    },
+  {
+    name: "nuts",
+    price: 160,
+    tag: "nb",
+    Incart: 0
+  },
 
-    {
-        name: 'coconut' , 
-        price : 160 , 
-        tag : 'ccb', 
-        Incart : 0
-    },
+  {
+    name: "coconut",
+    price: 160,
+    tag: "ccb",
+    Incart: 0
+  },
 
-    {
-        name: 'xeeera' , 
-        price : 150 , 
-        tag : 'zb', 
-        Incart : 0
-    },
+  {
+    name: "xeeera",
+    price: 150,
+    tag: "zb",
+    Incart: 0
+  },
 
-    {
-        name: 'naan khatai' , 
-        price : 150 , 
-        tag : 'nk', 
-        Incart : 0
-    },
+  {
+    name: "naan khatai",
+    price: 150,
+    tag: "nk",
+    Incart: 0
+  },
 
-    {
-        name: 'chicken' , 
-        price : 55 , 
-        tag : 'ch', 
-        Incart : 0
-    },
+  {
+    name: "chicken",
+    price: 55,
+    tag: "ch",
+    Incart: 0
+  },
 
-    {
-        name: 'samosa' , 
-        price : 40 , 
-        tag : 's', 
-        Incart : 0
-    },
+  {
+    name: "samosa",
+    price: 40,
+    tag: "s",
+    Incart: 0
+  },
 
-    {
-        name: 'mini pizza' , 
-        price : 110 , 
-        tag : 'mp', 
-        Incart : 0
-    },
+  {
+    name: "mini pizza",
+    price: 110,
+    tag: "mp",
+    Incart: 0
+  },
 
-    {
-        name: 'gralic bread' , 
-        price : 80 , 
-        tag : 'gb', 
-        Incart : 0
-    },
+  {
+    name: "gralic bread",
+    price: 80,
+    tag: "gb",
+    Incart: 0
+  },
 
-    {
-        name: 'sandwhich' , 
-        price : 75 , 
-        tag : 'sw', 
-        Incart : 0
-    },
+  {
+    name: "sandwhich",
+    price: 75,
+    tag: "sw",
+    Incart: 0
+  },
 
-    {
-        name: 'plain cake' , 
-        price : 210 , 
-        tag : 'pc', 
-        Incart : 0
-    },
+  {
+    name: "plain cake",
+    price: 210,
+    tag: "pc",
+    Incart: 0
+  },
 
-    {
-        name: 'eggs' , 
-        price : 120 , 
-        tag : 'e', 
-        Incart : 0
-    },
+  {
+    name: "eggs",
+    price: 120,
+    tag: "e",
+    Incart: 0
+  },
 
-    {
-        name: 'rusk' , 
-        price : 110 , 
-        tag : 'r', 
-        Incart : 0
-    },
+  {
+    name: "rusk",
+    price: 110,
+    tag: "r",
+    Incart: 0
+  },
 
-    {
-        name: 'Bread' , 
-        price : 50 , 
-        tag : 'b', 
-        Incart : 0
-    },
+  {
+    name: "Bread",
+    price: 50,
+    tag: "b",
+    Incart: 0
+  },
 
-    {
-        name: 'butter' , 
-        price : 210 , 
-        tag : 'bt', 
-        Incart : 0
-    },
+  {
+    name: "butter",
+    price: 210,
+    tag: "bt",
+    Incart: 0
+  },
 
-    {
-        name: 'maska bun' , 
-        price : 70 , 
-        tag : 'mb', 
-        Incart : 0
-    },
+  {
+    name: "maska bun",
+    price: 70,
+    tag: "mb",
+    Incart: 0
+  }
+];
 
-
-]
-
-for(let  i =0 ; i<button.length; i++){
-    button[i].addEventListener('click' , () => {
-     console.log('clicked');
-        cartnumbers(product[i]);
-        totalCost(product[i]);
-        dipslaycart();
-    })
-} 
-
-
-function onloadCartNumber(){
-    
-    let productNumber = localStorage.getItem('cartnumbers')
-
-    if (productNumber){
-        document.querySelector('.cart-item').textContent = productNumber;
-    }
+for (let i = 0; i < button.length; i++) {
+  button[i].addEventListener("click", () => {
+    console.log("clicked");
+    cartnumbers(product[i]);
+    totalCost(product[i]);
+    dipslaycart();
+  });
 }
 
-function cartnumbers(product){
-    
+function onloadCartNumber() {
+  let productNumber = localStorage.getItem("cartnumbers");
 
-    let productNumber = localStorage.getItem('cartnumbers')
-
-    productNumber = parseInt(productNumber)
-    if (productNumber){
-        localStorage.setItem('cartnumbers' , productNumber+ 1)
-        document.querySelector('.cart-item').textContent = productNumber + 1;
-    }else {
-    localStorage.setItem('cartnumbers' , 1)
-    document.querySelector('.cart-item').textContent = 1;
-}
-    setItem(product);
+  if (productNumber) {
+    document.querySelector(".cart-item").textContent = productNumber;
+  }
 }
 
-function setItem(product){
-    let cartitem = localStorage.getItem('product');
-    cartitem = JSON.parse(cartitem);
+function cartnumbers(product) {
+  let productNumber = localStorage.getItem("cartnumbers");
 
-    
-
-    if(cartitem != null){
-
-        if(cartitem[product.tag] == undefined){
-            cartitem ={
-                ...cartitem,
-                [product.tag]:product
-            }
-        }
-        cartitem[product.tag].Incart +=1;
-    }
-    else{
-
-        product.Incart = 1;
-        cartitem = {
-            [product.tag]:product
-        }
-    }
-        localStorage.setItem('product' , JSON.stringify(cartitem));
+  productNumber = parseInt(productNumber);
+  if (productNumber) {
+    localStorage.setItem("cartnumbers", productNumber + 1);
+    document.querySelector(".cart-item").textContent = productNumber + 1;
+  } else {
+    localStorage.setItem("cartnumbers", 1);
+    document.querySelector(".cart-item").textContent = 1;
+  }
+  setItem(product);
 }
 
-function totalCost(product){
-    let cost = localStorage.getItem('totalCost');
-    if (cost != null){
-        cost = parseInt(cost);
-        localStorage.setItem('totalCost' , cost + product.price );
+function setItem(product) {
+  let cartitem = localStorage.getItem("product");
+  cartitem = JSON.parse(cartitem);
+
+  if (cartitem != null) {
+    if (cartitem[product.tag] == undefined) {
+      cartitem = {
+        ...cartitem,
+        [product.tag]: product
+      };
     }
-    else{
-        localStorage.setItem('totalCost' , product.price);
-    }
+    cartitem[product.tag].Incart += 1;
+  } else {
+    product.Incart = 1;
+    cartitem = {
+      [product.tag]: product
+    };
+  }
+  localStorage.setItem("product", JSON.stringify(cartitem));
 }
 
-function dipslaycart(){
-    let cartitems = localStorage.getItem('product');
-    cartitems = JSON.parse(cartitems);
-    let productcontainer = document.querySelector('.product');
+function totalCost(product) {
+  let cost = localStorage.getItem("totalCost");
+  if (cost != null) {
+    cost = parseInt(cost);
+    localStorage.setItem("totalCost", cost + product.price);
+  } else {
+    localStorage.setItem("totalCost", product.price);
+  }
+}
 
-    console.log(cartitems);
+function dipslaycart() {
+  let cartitems = localStorage.getItem("product");
+  cartitems = JSON.parse(cartitems);
+  let productcontainer = document.querySelector(".product");
 
-    if(cartitems && productcontainer){
-        productcontainer.innerHTML ='';
-        Object.values(cartitems).map(item =>{
-            productcontainer.innerHTML += `
+  console.log(cartitems);
+
+  if (cartitems && productcontainer) {
+    productcontainer.innerHTML = "";
+    Object.values(cartitems).map(item => {
+      productcontainer.innerHTML += `
             <div class = "productsi">
             <i class="far fa-times-circle"></i>
-            <img src ="new project/${item.tag}.jpg">
+            <img src ="${item.tag}.jpg">
             <span>${item.name}</span>
              </div>
-             `
-                
-            
-        });
-    }
+             `;
+    });
+  }
 }
-
 
 onloadCartNumber();
 dipslaycart();
 
 function on() {
-    document.getElementById("overlay").style.display = "block";
-    console.log('clicked');
-  }
-  
-  function off() {
-    document.getElementById("overlay").style.display = "none";
-  }
+  document.getElementById("overlay").style.display = "block";
+  console.log("clicked");
+}
+
+function off() {
+  document.getElementById("overlay").style.display = "none";
+}
 
 // let shop = document.getElementById('but');
 // let card = document.getElementById('overlay');
@@ -310,5 +293,3 @@ function on() {
 //     console.log('clicked');
 //     card.classList.toogle('card');
 // })
-  
-  
