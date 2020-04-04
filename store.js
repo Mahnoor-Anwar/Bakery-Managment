ef6edd06949c77e26254fe9ead6c1c95eea41389
Shahzad Umar Baig <ShahzadUBaig@gmail.com>
2020-04-04 21:08:55 +0500

    I changed it

--- store.js
+++ store.js
@@ -7,7 +7,7 @@ var scroller = null;
 function ScrollFun(element) {
   destination = document.getElementById(element).offsetTop;
 
-  scroller = setTimeout(function() {
+  scroller = setTimeout(function () {
     ScrollFun(element);
   }, 7);
 
@@ -28,167 +28,167 @@ let product = [
     name: "you can shop your favourite item",
     price: 0,
     tag: "shop",
-    Incart: 0
+    Incart: 0,
   },
   {
     name: "chocolate cupcake",
     price: 90,
     tag: "cc",
-    Incart: 0
+    Incart: 0,
   },
 
   {
     name: "Vanila cupcake",
     price: 90,
     tag: "vc",
-    Incart: 0
+    Incart: 0,
   },
 
   {
     name: "oreo cupcake",
     price: 90,
     tag: "oc",
-    Incart: 0
+    Incart: 0,
   },
   {
     name: "strawberry cupcake",
     price: 90,
     tag: "sc",
-    Incart: 0
+    Incart: 0,
   },
 
   {
     name: "belgium cupcake",
     price: 90,
     tag: "bc",
-    Incart: 0
+    Incart: 0,
   },
 
   {
     name: "caramel cupcake",
     price: 90,
     tag: "crc",
-    Incart: 0
+    Incart: 0,
   },
 
   {
     name: "jamdot",
     price: 150,
     tag: "jd",
-    Incart: 0
+    Incart: 0,
   },
 
   {
     name: "chocolate ",
     price: 170,
     tag: "cb",
-    Incart: 0
+    Incart: 0,
   },
 
   {
     name: "nuts",
     price: 160,
     tag: "nb",
-    Incart: 0
+    Incart: 0,
   },
 
   {
     name: "coconut",
     price: 160,
     tag: "ccb",
-    Incart: 0
+    Incart: 0,
   },
 
   {
     name: "xeeera",
     price: 150,
     tag: "zb",
-    Incart: 0
+    Incart: 0,
   },
 
   {
     name: "naan khatai",
     price: 150,
     tag: "nk",
-    Incart: 0
+    Incart: 0,
   },
 
   {
     name: "chicken",
     price: 55,
     tag: "ch",
-    Incart: 0
+    Incart: 0,
   },
 
   {
     name: "samosa",
     price: 40,
     tag: "s",
-    Incart: 0
+    Incart: 0,
   },
 
   {
     name: "mini pizza",
     price: 110,
     tag: "mp",
-    Incart: 0
+    Incart: 0,
   },
 
   {
     name: "gralic bread",
     price: 80,
     tag: "gb",
-    Incart: 0
+    Incart: 0,
   },
 
   {
     name: "sandwhich",
     price: 75,
     tag: "sw",
-    Incart: 0
+    Incart: 0,
   },
 
   {
     name: "plain cake",
     price: 210,
     tag: "pc",
-    Incart: 0
+    Incart: 0,
   },
 
   {
     name: "eggs",
     price: 120,
     tag: "e",
-    Incart: 0
+    Incart: 0,
   },
 
   {
     name: "rusk",
     price: 110,
     tag: "r",
-    Incart: 0
+    Incart: 0,
   },
 
   {
     name: "Bread",
     price: 50,
     tag: "b",
-    Incart: 0
+    Incart: 0,
   },
 
   {
     name: "butter",
     price: 210,
     tag: "bt",
-    Incart: 0
+    Incart: 0,
   },
 
   {
     name: "maska bun",
     price: 70,
     tag: "mb",
-    Incart: 0
-  }
+    Incart: 0,
+  },
 ];
 
 for (let i = 0; i < button.length; i++) {
@@ -230,14 +230,14 @@ function setItem(product) {
     if (cartitem[product.tag] == undefined) {
       cartitem = {
         ...cartitem,
-        [product.tag]: product
+        [product.tag]: product,
       };
     }
     cartitem[product.tag].Incart += 1;
   } else {
     product.Incart = 1;
     cartitem = {
-      [product.tag]: product
+      [product.tag]: product,
     };
   }
   localStorage.setItem("product", JSON.stringify(cartitem));
@@ -262,11 +262,11 @@ function displaycart() {
 
   if (cartitems && productcontainer) {
     productcontainer.innerHTML = "";
-    Object.values(cartitems).map(item => {
+    Object.values(cartitems).map((item) => {
       productcontainer.innerHTML += `
             <div class="productss">
             <i class="far fa-times-circle"></i>
-            <img src="./Users/warda.anwer/Desktop/${item.tag}.jpg">
+            <img src="./${item.tag}.jpg">
             <span>${item.name}</span>
              </div>
              `;
@@ -276,7 +276,6 @@ function displaycart() {
 
 displaycart();
 onloadCartNumber();
-
 
 function on() {
   document.getElementById("overlay").style.display = "block";
